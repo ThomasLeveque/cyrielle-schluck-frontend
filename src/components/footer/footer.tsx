762449de790d2ca615f19774c8785b664b04fb2b
ThomasLeveque <thomasl5895@gmail.com>
2020-09-12 12:55:17 +0200

    Add footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FooterStyles } from './footer.styles';
+
+const Footer: React.FC = () => {
+  return (
+    <FooterStyles>
+      <p>
+        <span>Contactez-moi :</span> cyrielle.schluck@gmail.com
+      </p>
+    </FooterStyles>
+  );
+};
+
+export default Footer;
