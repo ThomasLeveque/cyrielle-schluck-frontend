11841b3fb0b999ee23442e579313d806d5db7d48
ThomasLeveque <thomasl5895@gmail.com>
2020-09-14 09:35:49 +0200

    Create NavLink component

---
+++ src/components/nav-link/nav-link.styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const NavLinkAStyles = styled.a`
+  font-size: 14px;
+  color: ${(props) => props.theme.colors.myBlack};
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+  cursor: pointer;
+
+  &:hover {
+    span {
+      transform: translateX(100%);
+    }
+  }
+
+  &.active {
+    span {
+      background-color: ${(props) => props.theme.colors.mainColor};
+    }
+  }
+
+  span {
+    margin-top: 8px;
+    height: 1px;
+    width: 100%;
+    background-color: ${(props) => props.theme.colors.myBlack};
+    transition: 0.5s ease-out;
+  }
+`;
---
+++ src/components/nav-link/nav-link.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import { NavLinkAStyles } from './nav-link.styles';
+
+interface NavLinkProps {
+  href: string;
+}
+
+const NavLink: React.FC<NavLinkProps> = ({ href, children }) => {
+  const router = useRouter();
+
+  let className = '';
+  if (router.pathname === href) {
+    className = 'active';
+  }
+
+  return (
+    <Link href={href}>
+      <NavLinkAStyles className={className}>
+        {children}
+        <span />
+      </NavLinkAStyles>
+    </Link>
+  );
+};
+
+export default NavLink;
