a8ecefc71b73c38b018f0d9184ea52cbc92216f4
ThomasLeveque <thomasl5895@gmail.com>
2020-09-14 09:34:58 +0200

    Update footer component

--- src/components/footer/footer.styles.ts
+++ src/components/footer/footer.styles.ts
@@ -1,3 +1,46 @@
 import styled from 'styled-components';
+import { rgba, border, math } from 'polished';
 
-export const FooterStyles = styled.footer``;
+export const FooterStyles = styled.footer`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: ${(props) => props.theme.vars.footerHeight}px;
+  background: ${(props) => props.theme.colors.white};
+  ${(props) =>
+    border(
+      'top',
+      `${props.theme.vars.footerBorderSize}px`,
+      'solid',
+      rgba(props.theme.colors.gray, 0.2)
+    )}
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 ${(props) => props.theme.vars.mSpace}px;
+
+  button {
+    font-size: 14px;
+    color: ${(props) => props.theme.colors.gray};
+    transition: 0.3s ease-out;
+    margin-top: ${(props) => math(`${props.theme.vars.footerBorderSize}px / 2`)};
+
+    &:hover {
+      color: ${(props) => props.theme.colors.myBlack};
+    }
+  }
+
+  p {
+    position: absolute;
+    left: 50%;
+    -webkit-transform: translateX(-50%);
+    transform: translateX(-50%);
+    margin-top: ${(props) => math(`${props.theme.vars.footerBorderSize}px / 2`)};
+
+    span {
+      color: ${(props) => props.theme.colors.gray};
+    }
+  }
+`;
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,12 +1,51 @@
 import React from 'react';
+import { motion, Variants, AnimatePresence } from 'framer-motion';
+
 import { FooterStyles } from './footer.styles';
 
-const Footer: React.FC = () => {
+const buttonVariants: Variants = {
+  initial: { opacity: 0, y: 5 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: 5 },
+};
+
+const buttonTransition = { ease: 'easeOut', duration: 0.3 };
+
+interface footerProps {
+  isProjectPage?: boolean;
+}
+
+const Footer: React.FC<footerProps> = ({ isProjectPage = false }) => {
   return (
     <FooterStyles>
-      <p>
-        <span>Contactez-moi :</span> cyrielle.schluck@gmail.com
-      </p>
+      <AnimatePresence>
+        {isProjectPage && (
+          <motion.button
+            key="prevButton"
+            variants={buttonVariants}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={buttonTransition}
+          >
+            {'< PROJET PRÉCÉDENT'}
+          </motion.button>
+        )}
+        <p>
+          <span>Contactez-moi :</span> cyrielle.schluck@gmail.com
+        </p>
+        {isProjectPage && (
+          <motion.button
+            key="nextButton"
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 10 }}
+            transition={buttonTransition}
+          >
+            {'PROJET SUIVANT >'}
+          </motion.button>
+        )}
+      </AnimatePresence>
     </FooterStyles>
   );
 };
