1149a26ed24c13e8e63d8945c89da36b560de2a6
ThomasLeveque <thomasl5895@gmail.com>
2020-09-14 09:38:10 +0200

    Update Index en poject page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,10 @@ import Layout from '@components/layout/layout';
 import { initializeApollo } from '@lib/apolloClient';
 import { ProjectsData, Project } from '@interfaces/project.interface';
 
+import { IndexStyles } from '@styles/pages/index.styles';
+import { H1Styles } from '@styles/texts/h1.styles';
+import { PStyles } from '@styles/texts/p.styles';
+
 export const ALL_PROJECTS_QUERY = gql`
   query {
     projects {
@@ -28,27 +32,32 @@ export const ALL_PROJECTS_QUERY = gql`
 interface HomeProps {}
 
 const Home: NextPage<HomeProps> = () => {
-  const { loading, data } = useQuery<ProjectsData>(ALL_PROJECTS_QUERY);
-
-  if (loading) {
-    return <div>loading...</div>;
-  }
+  const { data } = useQuery<ProjectsData>(ALL_PROJECTS_QUERY);
 
   const { projects } = data as ProjectsData;
 
   return (
     <Layout title="Home">
-      <ul>
-        {projects.map((project: Project) => (
-          <li key={project.id}>
-            <Link href={`projects/${project.id}`}>
-              <a>{project.name}</a>
-            </Link>
-            <p>Color: {project.color}</p>
-            <p>Desc: {project.shortDesc}</p>
-          </li>
-        ))}
-      </ul>
+      <IndexStyles>
+        <H1Styles>
+          <span>Cyrielle</span>,<br /> Designer UI<span>/</span>UX<span>.</span>
+        </H1Styles>
+        <PStyles letterSpacing={1}>
+          Designer UI & UX avec plus de 3 ans d’expérience, je mets l’utilisateur au centre de mon
+          travail ergonomique et graphique afin de lui assurer la meilleure expérience possible.
+        </PStyles>
+        <ul>
+          {projects.map((project: Project) => (
+            <li key={project.id}>
+              <Link href="projects/[projectId]" as={`projects/${project.id}`}>
+                <a>{project.name}</a>
+              </Link>
+              <p>Color: {project.color}</p>
+              <p>Desc: {project.shortDesc}</p>
+            </li>
+          ))}
+        </ul>
+      </IndexStyles>
     </Layout>
   );
 };
--- src/pages/projects/[projectId].tsx
+++ src/pages/projects/[projectId].tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import { ThemeContext } from 'styled-components';
 
 import Layout from '@components/layout/layout';
+
+import { H1Styles } from '@styles/texts/h1.styles';
 
 interface ProjectProps {}
 
@@ -10,9 +13,13 @@ const Project: NextPage<ProjectProps> = () => {
   const router = useRouter();
   const { projectId } = router.query;
 
+  const theme = useContext(ThemeContext);
+
   return (
     <Layout title={`Project: ${projectId}`}>
-      <h1>Project: {projectId}</h1>
+      <H1Styles fontSize={60} lineHeight={70} mb={0} color={theme.colors.myWhite}>
+        Project: {projectId}
+      </H1Styles>
     </Layout>
   );
 };
