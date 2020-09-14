5c70643598ce0de3cc70da04e1c21c7f521667ac
ThomasLeveque <thomasl5895@gmail.com>
2020-09-14 09:35:36 +0200

    Remove header and footer from layout

--- src/components/layout/layout.styles.ts
+++ src/components/layout/layout.styles.ts
@@ -1,3 +1,7 @@
 import styled from 'styled-components';
+import { math } from 'polished';
 
-export const LayoutStyles = styled.main``;
+export const LayoutStyles = styled.main`
+  padding: 0 ${(props) => props.theme.vars.lSpace}px
+    ${(props) => math(`${props.theme.vars.mSpace}px + ${props.theme.vars.footerHeight}px`)};
+`;
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import Head from 'next/head';
 import { motion } from 'framer-motion';
 
-import Header from '@components/header/header';
-import Footer from '@components/footer/footer';
-
 import { LayoutStyles } from './layout.styles';
 
 interface LayoutProps {
@@ -13,21 +10,17 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ title, children }) => {
   return (
-    <>
+    <LayoutStyles
+      as={motion.main}
+      exit={{ opacity: 0 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
       <Head>
         <title>{title}</title>
       </Head>
-      <Header />
-      <LayoutStyles
-        as={motion.main}
-        exit={{ opacity: 0 }}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-      >
-        {children}
-      </LayoutStyles>
-      <Footer />
-    </>
+      {children}
+    </LayoutStyles>
   );
 };
 
