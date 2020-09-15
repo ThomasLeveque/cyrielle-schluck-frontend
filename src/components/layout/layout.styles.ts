020d8853d088205caaa12faf672296aeaf134406
ThomasLeveque <thomasl5895@gmail.com>
2020-09-15 11:45:17 +0200

    Fix layout

--- src/components/layout/layout.styles.ts
+++ src/components/layout/layout.styles.ts
@@ -2,10 +2,9 @@ import styled from 'styled-components';
 import { math } from 'polished';
 
 export const LayoutStyles = styled.main`
-  width: 100%;
-  max-width: ${(props) => props.theme.vars.appMaxWidth}px;
+  max-width: 100%;
+  width: ${(props) => props.theme.vars.appMaxWidth}px;
   margin: 0 auto;
-  box-sizing: content-box;
   padding: 0 ${(props) => props.theme.vars.lSpace}px
     ${(props) => math(`${props.theme.vars.mSpace}px + ${props.theme.vars.footerHeight}px`)};
 `;
